76aca6b517fbcc87cad3f00e3e4223338cd597dd
Michail <mihasik323@gmail.com>
2020-07-12 17:39:15 +0300

    added news item page

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,5 @@
 $(function() {
 
-    var mixer = mixitup('.products__inner-box');
-    // $('.rate-star').rateYo({
-    //     rating: 5
-    // });
     $('.rate-star').rateYo({
         rating: 5,
         starWidth: "12px",
@@ -17,4 +13,32 @@ $(function() {
         slidesToScroll: 4,
         autoplay: 3000,
     });
+
+    $('.js-range-slider').ionRangeSlider({
+
+        type: "double",
+        grid: false,
+        min: 0,
+        max: 1000,
+        from: 0,
+        to: 600,
+        prefix: "$"
+
+
+    });
+
+    $('.icon-th-list').on('click', function() {
+        $('.products__item').addClass('list');
+        $('.icon-th-list').addClass('active');
+        $('.icon-th-large').removeClass('active');
+
+    });
+    $('.icon-th-large').on('click', function() {
+        $('.products__item').removeClass('list');
+        $('.icon-th-large').addClass('active');
+        $('.icon-th-list').removeClass('active');
+    });
+
+    var mixer = mixitup('.products__inner-box');
+
 });
