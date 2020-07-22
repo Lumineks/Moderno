e1590434d8ece1e92ebb7e0037e713ed9c80bfc7
Lumineks <mihasik323@gmail.com>
2020-07-22 16:32:10 +0300

    moderno - last lesson

--- app/js/main.js
+++ app/js/main.js
@@ -65,6 +65,18 @@ $(function() {
         return false;
     });
 
+    $('.profile__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+        $('.profile__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.profile__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
+    $('input[type="file"], select').styler();
+
+
     var mixer = mixitup('.products__inner-box');
 
 });
